2d8e569a408393ca54e704059b4f6fe7cf0591c4
JuanDiazCortez <mdc_marcelo@yahoo.com>
2022-06-25 18:59:16 -0300

    base64 show

--- src/components/BrowserRow/BrowserRowHooks.js
+++ src/components/BrowserRow/BrowserRowHooks.js
@@ -26,28 +26,30 @@ export const isIMG = (attach) => {
   );
 };
 
-
 /**
  * Display a base64 URL inside an iframe in another window.
  */
- function debugBase64(base64URL){
+function debugBase64(base64URL) {
   var win = window.open();
-  win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+  win.document.write(
+    '<iframe src="' +
+      base64URL +
+      '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
+  );
 }
 
 // e.g This will open an image in a new window
 /**
  * Display a base64 URL inside an iframe in another window.
  */
- function showDocBase64(base64URL){
+function showDocBase64(base64URL) {
   let win = window.open();
-  win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+  win.document.write(
+    '<iframe src="' +
+      base64URL +
+      '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
+  );
 }
-
-
-
-
-
 
 export const onClickDownLoadAttach = (ev, attach) => {
   const { target } = ev;
