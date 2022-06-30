9eaaf1b5e8089427b319a273f71e18b0597b4150
Juan <you@example.com>
2022-06-30 01:25:45 -0300

    contstants

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
 var PORT_BACKEND = "5100";
 const URL_DATABASE = "192.168.0.120";
 var URL_PROTOCOL = "http";
-const URL_BACKEND = "192.168.0.120";
+const URL_BACKEND = "192.168.0.100";
 
 const DEFAULT_POST_HEADER = {
   method: "POST",
